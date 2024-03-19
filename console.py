563980b2f36269bcd928fef3da2908b4d2fee3da
TarekM007 <tarekmoustafa36@gmail.com>
2024-03-19 14:31:13 -0700

    2

--- console.py
+++ console.py
@@ -2,7 +2,6 @@
 """ Console Module """
 import cmd
 import sys
-import re
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -11,7 +10,6 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import Column, String
 
 
 class HBNBCommand(cmd.Cmd):
@@ -116,42 +114,51 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        pattern = """(^\w+)((?:\s+\w+=[^\s]+)+)?"""
-        m = re.match(pattern, args)
-        args = [s for s in m.groups() if s] if m else []
-
+        """ Create an object of any class """
         if not args:
             print("** class name missing **")
             return
 
-        className = args[0]
+        args_list = args.split()
+        class_name = args_list[0]
+        params = args_list[1:]
 
-        if className not in HBNBCommand.classes:
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
 
-        kwargs = dict()
-        if len(args) > 1:
-            params = args[1].split(" ")
-            params = [param for param in params if param]
-            for param in params:
-                [name, value] = param.split("=")
-                if value[0] == '"' and value[-1] == '"':
-                    value = value[1:-1].replace('_', ' ')
-                elif '.' in value:
+        param_dict = {}
+
+        for param in params:
+            parts = param.split('=')
+            if len(parts) != 2:
+                print(f"Invalid parameter: {param}. Skipping...")
+                continue
+
+            key, value = parts[0], parts[1]
+            key = key.replace('_', ' ')
+
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1].replace('\\"', '"')
+            elif '.' in value:
+                try:
                     value = float(value)
-                else:
+                except ValueError:
+                    print(f"Invalid float value: {value}. Skipping...")
+                    continue
+            else:
+                try:
                     value = int(value)
-                kwargs[name] = value
+                except ValueError:
+                    print(f"Invalid integer value: {value}. Skipping...")
+                    continue
 
-        new_instance = HBNBCommand.classes[className]()
-        
-        for attrName, attrValue in kwargs.items():
-            setattr(new_instance, attrName, attrValue) 
+            param_dict[key] = value
 
-        new_instance.save()
+        new_instance = HBNBCommand.classes[class_name](**param_dict)
+        storage.save()
         print(new_instance.id)
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -233,7 +240,7 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all().items():
+            for k, v in storage._FileStorage__objects.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
