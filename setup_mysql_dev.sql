f313d1c60b4a84827c0f1a9909b8681ac3c48768
TarekM007 <tarekmoustafa36@gmail.com>
2024-03-19 13:09:40 -0700

    3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- A script that prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREAE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES  ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
