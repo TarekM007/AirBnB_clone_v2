5367171d894bdf73de37c0c296b2c976425a259a
TarekM007 <tarekmoustafa36@gmail.com>
2024-03-20 12:21:59 -0700

    6

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,17 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, datetime
+from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable = False)
+    created_at = Column(datetime,default = datetime.utcnow(), nullable=False)
+    updated_at = Column(datetime,default = datetime.utcnow(), nullable=False)
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
@@ -49,4 +56,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
+    def delete(self):
+        """delete the current instance from storage"""
+        from models import storage
+        storage.delete(self)
