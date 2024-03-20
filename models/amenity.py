ab73978cffce63c80571ce54e228cb79c3884277
TarekM007 <tarekmoustafa36@gmail.com>
2024-03-20 15:05:37 -0700

    6

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" State Module for the HBNB project """
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """class for amenities"""
+    __tablename__ = 'amenities'
+    env_var = getenv("HBNB_TYPE_STORAGE")
+    if env_var == "db":
+        name = Column(String(128), nullable=False)
+        place_amenity = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
+    else:
+        name = ""
