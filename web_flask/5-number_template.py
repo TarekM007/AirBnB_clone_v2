08e0f4230843003f907dd971d76237854de6a757
TarekM007 <tarekmoustafa36@gmail.com>
2024-05-16 13:54:08 -0700

    5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display_hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def desplay_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_cText(text):
+    text = text.replace("_", " ")
+    return "C %s" % (text)
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_pythonText(text='is cool'):
+    text = text.replace("_", " ")
+    return "Python %s" % (text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_if_int(n):
+    return "%d is a number" % (n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return number_template("5-number.html", num=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
