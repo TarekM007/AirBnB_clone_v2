537e877abbaacb287b5791d8bb9c071cb1b1e586
TarekM007 <tarekmoustafa36@gmail.com>
2024-05-16 14:10:22 -0700

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,7 +36,7 @@ def display_if_int(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return number_template("5-number.html", num=n)
+    return render_template("5-number.html", num=n)
 
 
 if __name__ == '__main__':
